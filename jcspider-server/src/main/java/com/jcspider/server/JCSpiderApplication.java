2e5656f73476dc1dbff59a9d3fa43b457bbf347d
zhuang.hu <zhuang.hu@bianlifeng.com>
2019-05-29 10:39:27 +0800

    init

---
+++ jcspider-server/src/main/java/com/jcspider/server/JCSpiderApplication.java
@@ -0,0 +1,19 @@
+package com.jcspider.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuang.hu
+ * @since 28 五月 2019
+ */
+@SpringBootApplication
+public class JCSpiderApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCSpiderApplication.class, args);
+    }
+
+
+}
