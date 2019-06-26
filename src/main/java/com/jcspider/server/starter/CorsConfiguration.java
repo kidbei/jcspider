bf0fd90986e9c610b36f2d265e253937d2e909f6
zhuang.hu <zhuang.hu@wormpex.com>
2019-06-26 13:45:51 +0800

    format

---
+++ src/main/java/com/jcspider/server/starter/CorsConfiguration.java
@@ -0,0 +1,8 @@
+package com.jcspider.server.starter;
+
+/**
+ * @author zhuang.hu
+ * @since 25 June 2019
+ */
+public class CorsConfiguration {
+}
---
+++ src/main/java/com/jcspider/server/web/api/LoginController.java
@@ -0,0 +1,8 @@
+package com.jcspider.server.web.api;
+
+/**
+ * @author zhuang.hu
+ * @since 25 June 2019
+ */
+public class LoginController {
+}
